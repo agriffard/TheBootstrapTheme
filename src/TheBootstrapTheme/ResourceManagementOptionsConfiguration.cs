a8978db88cf16f9e84e6f277fa428c90c391c57d
Ferenc Győri <ferenc.gyori@lombiq.com>
2021-09-27 20:05:21 +0200

    Updating to 1.0.0

--- src/TheBootstrapTheme/ResourceManagementOptionsConfiguration.cs
+++ src/TheBootstrapTheme/ResourceManagementOptionsConfiguration.cs
@@ -1,27 +1,30 @@
+using Microsoft.Extensions.Options;
 using OrchardCore.ResourceManagement;
 
 namespace TheBootstrapTheme.OrchardCore
 {
-    public class ResourceManifest : IResourceManifestProvider
+    public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
     {
-        public void BuildManifests(IResourceManifestBuilder builder)
-        {
-            var manifest = builder.Add();
+        private static readonly ResourceManifest _manifest = new();
 
-            manifest
+        static ResourceManagementOptionsConfiguration()
+        {
+            _manifest
                 .DefineStyle("TheBootstrapTheme-bootstrap-oc")
                 .SetUrl("~/TheBootstrapTheme/css/bootstrap-oc.min.css", "~/TheBootstrapTheme/css/bootstrap-oc.css")
                 .SetVersion("1.0.0");
 
-            manifest
+            _manifest
                 .DefineStyle("navbar-top-fixed")
                 .SetUrl("~/TheBootstrapTheme/css/navbar-top-fixed.css")
                 .SetVersion("1.0.0");
-            
-            manifest
+
+            _manifest
                 .DefineStyle("sticky-footer")
                 .SetUrl("~/TheBootstrapTheme/css/sticky-footer.css")
-                .SetVersion("1.0.0");				
+                .SetVersion("1.0.0");
         }
+
+        public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
     }
 }
