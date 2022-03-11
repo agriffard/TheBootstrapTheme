9957b9634da8fecb0dbbf24a29d0354139daede7
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-11 14:08:57 +0100

    Fixing repo link

--- src/TheBootstrapTheme/Manifest.cs
+++ src/TheBootstrapTheme/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.DisplayManagement.Manifest;
 [assembly: Theme(
     Name = "The Bootstrap Theme",
     Author = "Antoine Griffard",
-    Website = "https://github.com/agriffard/TheBootstrapTheme.OrchardCore",
+    Website = "https://github.com/agriffard/TheBootstrapTheme",
     Version = "1.0.0",
     Description = "Bootstrap theme",
     Tags = new[] { "Bootstrap" }
